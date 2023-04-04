7b4b4c3c2c09688790316e9fa7c408c97c9eae1e
Chihao1702 <117892988+Chihao1702@users.noreply.github.com>
2023-04-04 15:12:19 +0700

    Create CTDL&GT_Selection_Sort_2.cpp

---
+++ CTDL&GT_Selection_Sort_2.cpp
@@ -0,0 +1,8 @@
+//https://freetuts.net/thuat-toan-sap-xep-chon-selection-sort-2931.html
+Nhập vào số lượng phần tử có trong mảng : 
+a[0] = 7
+a[1] = 5
+a[2] = 4
+a[3] = 2
+Mảng sau khi sắp xếp :
+   2    4    5    7
