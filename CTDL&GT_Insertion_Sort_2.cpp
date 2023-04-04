012d263c2954337f73798d559ff3f9af2fc8714a
Chihao1702 <117892988+Chihao1702@users.noreply.github.com>
2023-04-04 15:32:19 +0700

    Create CTDL&GT_Insertion_Sort_2.cpp

---
+++ CTDL&GT_Insertion_Sort_2.cpp
@@ -0,0 +1,5 @@
+//https://cafedev.vn/thuat-toan-insertion-sort-gioi-thieu-chi-tiet-va-code-vi-du-tren-nhieu-ngon-ngu-lap-trinh/
+Linked List before sorting
+30  3  4  20  5
+Linked List after sorting
+3  4  5  20  30
